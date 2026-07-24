27891f7ed4a0cf08fec0689ab8c124199aa53fc6
Lucke0302 <Lucke0302@gmail.com>
2026-07-24 19:00:05 -0300

    fix: teste textura

--- src/main/java/com/lucas/arch/client/model/AllosaurusModel.java
+++ src/main/java/com/lucas/arch/client/model/AllosaurusModel.java
@@ -4,7 +4,7 @@ import com.geckolib.model.GeoModel;
 import com.geckolib.renderer.base.GeoRenderState;
 import com.lucas.arch.ArcheologyReimagined;
 import com.lucas.arch.entity.AllosaurusEntity;
-import com.lucas.arch.renderer.AllosaurusRenderer;
+
 import net.minecraft.resources.Identifier;
 
 public class AllosaurusModel extends GeoModel<AllosaurusEntity> {
@@ -15,13 +15,7 @@ public class AllosaurusModel extends GeoModel<AllosaurusEntity> {
 
     @Override
     public Identifier getTextureResource(GeoRenderState renderState) {
-        if (renderState.isBaby) {
-            return ArcheologyReimagined.id("textures/entity/allosaurus_baby.png");
-        }
-        Boolean isMale = renderState.getData(AllosaurusRenderer.IS_MALE_KEY);
-        return (isMale != null && isMale)
-                ? ArcheologyReimagined.id("textures/entity/allosaurus_male.png")
-                : ArcheologyReimagined.id("textures/entity/allosaurus_female.png");
+        return null;
     }
 
     @Override
