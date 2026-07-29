5ee124b32bb5b45c8ac2c84d90329e39bc9f7405
Lucke0302 <Lucke0302@gmail.com>
2026-07-29 14:38:43 -0300

    fix: ajustes pro jade

--- src/main/java/com/lucas/arch/compat/jade/ArchJadePlugin.java
+++ src/main/java/com/lucas/arch/compat/jade/ArchJadePlugin.java
@@ -3,10 +3,12 @@ package com.lucas.arch.compat.jade;
 import com.lucas.arch.block.AllosaurusEggBlock;
 import com.lucas.arch.block.PachycephalosaurusEggBlock;
 import com.lucas.arch.block.ParasaurolophusEggBlock;
+import com.lucas.arch.block.QuetzalcoatlusEggBlock;
 import com.lucas.arch.block.SpinosaurusEggBlock;
 import com.lucas.arch.entity.AllosaurusEntity;
 import com.lucas.arch.entity.PachycephalosaurusEntity;
 import com.lucas.arch.entity.ParasaurolophusEntity;
+import com.lucas.arch.entity.QuetzalcoatlusEntity;
 import com.lucas.arch.entity.SpinosaurusEntity;
 import snownee.jade.api.IWailaClientRegistration;
 import snownee.jade.api.IWailaCommonRegistration;
@@ -23,11 +25,13 @@ public class ArchJadePlugin implements IWailaPlugin {
         registration.registerBlockDataProvider(AllosaurusEggServerProvider.INSTANCE, PachycephalosaurusEggBlock.class);
         registration.registerBlockDataProvider(AllosaurusEggServerProvider.INSTANCE, SpinosaurusEggBlock.class);
         registration.registerBlockDataProvider(AllosaurusEggServerProvider.INSTANCE, ParasaurolophusEggBlock.class);
+        registration.registerBlockDataProvider(AllosaurusEggServerProvider.INSTANCE, QuetzalcoatlusEggBlock.class);
         
         registration.registerEntityDataProvider(AllosaurusServerProvider.INSTANCE, AllosaurusEntity.class);
         registration.registerEntityDataProvider(AllosaurusServerProvider.INSTANCE, PachycephalosaurusEntity.class);
         registration.registerEntityDataProvider(AllosaurusServerProvider.INSTANCE, SpinosaurusEntity.class);
         registration.registerEntityDataProvider(AllosaurusServerProvider.INSTANCE, ParasaurolophusEntity.class);
+        registration.registerEntityDataProvider(AllosaurusServerProvider.INSTANCE, QuetzalcoatlusEntity.class);
     }
 
     @Override
@@ -37,10 +41,12 @@ public class ArchJadePlugin implements IWailaPlugin {
         registration.registerBlockComponent(AllosaurusEggClientProvider.INSTANCE, PachycephalosaurusEggBlock.class);
         registration.registerBlockComponent(AllosaurusEggClientProvider.INSTANCE, SpinosaurusEggBlock.class);
         registration.registerBlockComponent(AllosaurusEggClientProvider.INSTANCE, ParasaurolophusEggBlock.class);
+        registration.registerBlockComponent(AllosaurusEggClientProvider.INSTANCE, QuetzalcoatlusEggBlock.class);
 
         registration.registerEntityComponent(AllosaurusClientProvider.INSTANCE, AllosaurusEntity.class);
         registration.registerEntityComponent(AllosaurusClientProvider.INSTANCE, PachycephalosaurusEntity.class);
         registration.registerEntityComponent(AllosaurusClientProvider.INSTANCE, SpinosaurusEntity.class);
         registration.registerEntityComponent(AllosaurusClientProvider.INSTANCE, ParasaurolophusEntity.class);
+        registration.registerEntityComponent(AllosaurusClientProvider.INSTANCE, QuetzalcoatlusEntity.class);
     }
 }
