08e69694a56d7f08d36d9e1184035f97cc4fc19a
Lucke0302 <Lucke0302@gmail.com>
2026-08-11 14:35:57 -0300

    feat: detalhes do dimorphodon

--- src/main/java/com/lucas/arch/block/entity/DimorphodonEggBlockEntity.java
+++ src/main/java/com/lucas/arch/block/entity/DimorphodonEggBlockEntity.java
@@ -3,16 +3,29 @@ package com.lucas.arch.block.entity;
 import com.lucas.arch.entity.DimorphodonEntity;
 import com.lucas.arch.registry.ModBlockEntities;
 import com.lucas.arch.registry.ModEntities;
+
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class DimorphodonEggBlockEntity extends AbstractDinosaurEggBlockEntity<DimorphodonEntity> {
+    
     public DimorphodonEggBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntities.DIMORPHODON_EGG_BE, pos, state);
     }
 
-    @Override protected float getHatchMultiplier() { return 0.7f; }
-    @Override protected int getMaxHeatSources() { return 4; }
-    @Override protected EntityType<DimorphodonEntity> getEntityType() { return ModEntities.DIMORPHODON; }
+    @Override 
+    protected float getHatchMultiplier() { 
+        return 0.7f; 
+    }
+
+    @Override 
+    protected int getMaxHeatSources() { 
+        return 4; 
+    }
+
+    @Override 
+    protected EntityType<DimorphodonEntity> getEntityType() { 
+        return ModEntities.DIMORPHODON; 
+    }
 }
--- src/main/java/com/lucas/arch/compat/jade/ArchJadePlugin.java
+++ src/main/java/com/lucas/arch/compat/jade/ArchJadePlugin.java
@@ -1,11 +1,13 @@
 package com.lucas.arch.compat.jade;
 
 import com.lucas.arch.block.AllosaurusEggBlock;
+import com.lucas.arch.block.DimorphodonEggBlock;
 import com.lucas.arch.block.PachycephalosaurusEggBlock;
 import com.lucas.arch.block.ParasaurolophusEggBlock;
 import com.lucas.arch.block.QuetzalcoatlusEggBlock;
 import com.lucas.arch.block.SpinosaurusEggBlock;
 import com.lucas.arch.entity.AllosaurusEntity;
+import com.lucas.arch.entity.DimorphodonEntity;
 import com.lucas.arch.entity.PachycephalosaurusEntity;
 import com.lucas.arch.entity.ParasaurolophusEntity;
 import com.lucas.arch.entity.QuetzalcoatlusEntity;
@@ -17,7 +19,6 @@ import snownee.jade.api.WailaPlugin;
 
 @WailaPlugin
 public class ArchJadePlugin implements IWailaPlugin {
-
     @Override
     public void register(IWailaCommonRegistration registration) {
         // Registro Server-Side
@@ -26,12 +27,14 @@ public class ArchJadePlugin implements IWailaPlugin {
         registration.registerBlockDataProvider(AllosaurusEggServerProvider.INSTANCE, SpinosaurusEggBlock.class);
         registration.registerBlockDataProvider(AllosaurusEggServerProvider.INSTANCE, ParasaurolophusEggBlock.class);
         registration.registerBlockDataProvider(AllosaurusEggServerProvider.INSTANCE, QuetzalcoatlusEggBlock.class);
+        registration.registerBlockDataProvider(AllosaurusEggServerProvider.INSTANCE, DimorphodonEggBlock.class);
         
         registration.registerEntityDataProvider(AllosaurusServerProvider.INSTANCE, AllosaurusEntity.class);
         registration.registerEntityDataProvider(AllosaurusServerProvider.INSTANCE, PachycephalosaurusEntity.class);
         registration.registerEntityDataProvider(AllosaurusServerProvider.INSTANCE, SpinosaurusEntity.class);
         registration.registerEntityDataProvider(AllosaurusServerProvider.INSTANCE, ParasaurolophusEntity.class);
         registration.registerEntityDataProvider(AllosaurusServerProvider.INSTANCE, QuetzalcoatlusEntity.class);
+        registration.registerEntityDataProvider(AllosaurusServerProvider.INSTANCE, DimorphodonEntity.class);
     }
 
     @Override
@@ -42,11 +45,13 @@ public class ArchJadePlugin implements IWailaPlugin {
         registration.registerBlockComponent(AllosaurusEggClientProvider.INSTANCE, SpinosaurusEggBlock.class);
         registration.registerBlockComponent(AllosaurusEggClientProvider.INSTANCE, ParasaurolophusEggBlock.class);
         registration.registerBlockComponent(AllosaurusEggClientProvider.INSTANCE, QuetzalcoatlusEggBlock.class);
+        registration.registerBlockComponent(AllosaurusEggClientProvider.INSTANCE, DimorphodonEggBlock.class);
 
         registration.registerEntityComponent(AllosaurusClientProvider.INSTANCE, AllosaurusEntity.class);
         registration.registerEntityComponent(AllosaurusClientProvider.INSTANCE, PachycephalosaurusEntity.class);
         registration.registerEntityComponent(AllosaurusClientProvider.INSTANCE, SpinosaurusEntity.class);
         registration.registerEntityComponent(AllosaurusClientProvider.INSTANCE, ParasaurolophusEntity.class);
         registration.registerEntityComponent(AllosaurusClientProvider.INSTANCE, QuetzalcoatlusEntity.class);
+        registration.registerEntityComponent(AllosaurusClientProvider.INSTANCE, DimorphodonEntity.class);
     }
 }
